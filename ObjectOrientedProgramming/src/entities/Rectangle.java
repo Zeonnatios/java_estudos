98f15baff5de10f283c3d8fae2eed1dafc60003e
Matheus Antonio <matheusantonio232@gmail.com>
2022-04-26 17:49:10 -0300

    rectangle operations program

--- ObjectOrientedProgramming/src/entities/Rectangle.java
+++ ObjectOrientedProgramming/src/entities/Rectangle.java
@@ -1,4 +1,20 @@
 package entities;
 
 public class Rectangle {
+
+    public double width;
+    public double height;
+
+    public double calculateArea(){
+        return width * height;
+    }
+
+    public double calculatePerimeter(){
+        return 2 * (width + height);
+    }
+
+    public double calculateDiagonal() {
+        return Math.sqrt(width * width + height * height);
+    }
+
 }
