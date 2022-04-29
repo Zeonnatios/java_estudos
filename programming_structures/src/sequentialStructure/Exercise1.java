3babbc7ad7d5edfd06e3157b1fc7455f44d6d1c3
Matheus Antonio <matheusantonio232@gmail.com>
2022-04-29 15:13:24 -0300

    exercise 1

---
+++ programming_structures/src/sequentialStructure/Exercise1.java
@@ -0,0 +1,25 @@
+package sequentialStructure;
+
+import java.util.Scanner;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the first value: ");
+        int firstValue = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Enter the second value: ");
+        int secondValue = sc.nextInt();
+        sc.nextLine();
+
+        int total = firstValue + secondValue;
+
+        System.out.println("Out: " + total);
+    }
+
+
+}
