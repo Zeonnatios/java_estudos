306fe5f6e6948fc14f3592b9e10cfadb655b962d
Matheus Antonio <matheusantonio232@gmail.com>
2022-04-29 15:24:20 -0300

    exercise 3

--- programming_structures/src/sequentialStructure/Exercise3.java
+++ programming_structures/src/sequentialStructure/Exercise3.java
@@ -1,2 +1,29 @@
-package sequentialStructure;public class Exercise3 {
+package sequentialStructure;
+
+import java.util.Scanner;
+
+public class Exercise3 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first value: ");
+        int valueA = sc.nextInt();
+
+        System.out.println("Enter second value: ");
+        int valueB = sc.nextInt();
+
+        System.out.println("Enter third value: ");
+        int valueC = sc.nextInt();
+
+        System.out.println("Enter fourth value: ");
+        int valueD = sc.nextInt();
+
+        int result = valueA * valueB - valueC * valueD;
+
+        System.out.println("Difference: " + result);
+
+
+    }
 }
